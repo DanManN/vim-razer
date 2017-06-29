998a51408635a5cb87bd90f18950eacf1a70db46
DanManN <dnahimov@gmail.com>
2017-06-29 14:05:45 -0500

    more pretty colors

--- plugin/mode_colors.py
+++ plugin/mode_colors.py
@@ -50,21 +50,50 @@ def updateKeyColors(mode):
         #print("vim-razer: error: no keyboard found")
         return 0 
 
+    # get the keyboard color matrix
     mat = keyboard.fx.advanced.matrix
-    
+
     # helper function to set main color
     def setBaseColor(color):
         for key, coord in keylayout.items():
             mat[coord] = color
-
+    
+    # set colors depending on mode
     if mode == "insert":
-        setBaseColor(colors.BLUE)    
+        setBaseColor(colors.BLUE)
     elif mode == "replace":
-        setBaseColor(colors.RED)    
-    elif mode == "visual":
-        setBaseColor(colors.ORANGE)    
+        setBaseColor(colors.RED)
     else:
-       setBaseColor(colors.GREEN)    
+        if mode == "visual":
+            setBaseColor(colors.ORANGE)
+        else:
+            setBaseColor(colors.GREEN)
+        mat[keylayout['i']] = colors.BLUE
+        mat[keylayout['o']] = colors.BLUE
+        mat[keylayout['a']] = colors.BLUE
+        mat[keylayout['s']] = colors.BLUE
+        mat[keylayout['v']] = colors.ORANGE
+        mat[keylayout['h']] = colors.YELLOW
+        mat[keylayout['j']] = colors.YELLOW
+        mat[keylayout['k']] = colors.YELLOW
+        mat[keylayout['l']] = colors.YELLOW
+        mat[keylayout['w']] = colors.YELLOW
+        mat[keylayout['b']] = colors.YELLOW
+        mat[keylayout['y']] = colors.RED
+        mat[keylayout['d']] = colors.RED
+        mat[keylayout['p']] = colors.RED
+        mat[keylayout['r']] = colors.RED
+        mat[keylayout['x']] = colors.RED
+        mat[keylayout['u']] = colors.RED
+        mat[keylayout['left_ctrl']] = colors.RED
+        mat[keylayout['left_shift']] = colors.RED
+        mat[keylayout['right_ctrl']] = colors.RED
+        mat[keylayout['right_shift']] = colors.RED
 
+
+    # esc always goes to normal mode
+    mat[keylayout['esc']] = colors.GREEN
+
+    # draw colors on keyboard
     keyboard.fx.advanced.draw()
     return 0
